0322e33201bd05ac11bbaa7f0c44764382ee37fa
Dominik Korpusik <dominik.korpusik@gmail.com>
2023-01-09 22:55:21 +0100

    Update email_sender.py

--- email_sender.py
+++ email_sender.py
@@ -6,15 +6,15 @@ from pathlib import Path
 html = Template(Path('index.html').read_text())
 email = EmailMessage()
 email['from'] = 'Dominik Korpusik'
-email['to'] = 'dominik.korpusik@gmail.com'
+email['to'] = 'email@gmail.com' #FILL
 email['subject'] = 'You won a 1,000,000 dollrs!'
 
 email.set_content(html.substitute({'name': 'TinTin', 'age': 20}), 'html')
 
 
 with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
-    email_address = 'kowalczykaa37@gmail.com'
-    email_pass = 'ajxavpjeaxnoflyc'
+    email_address = '' #FILL
+    email_pass = '' #FILL
     smtp.ehlo()
     smtp.starttls()
     smtp.login(email_address, email_pass)
