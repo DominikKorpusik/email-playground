58d6628c5f43f227b8955940ec15e9ed8b76f3a8
Dominik Korpusik <dominik.korpusik@gmail.com>
2023-01-14 17:25:52 +0000

    Adding .gitignore, Changing configuration

--- email_sender.py
+++ email_sender.py
@@ -1,3 +1,5 @@
+# config.py : where I keep my keys as constants
+import config
 import smtplib
 from email.message import EmailMessage
 from string import Template
@@ -6,15 +8,15 @@ from pathlib import Path
 html = Template(Path('index.html').read_text())
 email = EmailMessage()
 email['from'] = 'Dominik Korpusik'
-email['to'] = 'dominik.korpusik@gmail.com'
+email['to'] = config.TO_EMAIL
 email['subject'] = 'You won a 1,000,000 dollrs!'
 
 email.set_content(html.substitute({'name': 'TinTin', 'age': 20}), 'html')
 
 
 with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
-    email_address = 'kowalczykaa37@gmail.com'
-    email_pass = 'ajxavpjeaxnoflyc'
+    email_address = config.EMAIL_ADDRESS
+    email_pass = config.EMAIL_PASSWORD
     smtp.ehlo()
     smtp.starttls()
     smtp.login(email_address, email_pass)
